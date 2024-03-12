1cefd2062b21fc8b18fe3c468a5688e60e57838b
Benedettopana <benedetto.pana@gmail.com>
2024-03-12 15:19:23 +0100

    [add]img carousel

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const { createApp} = Vue;
+const {createApp} = Vue;
 
 createApp({
     data(){
@@ -7,35 +7,53 @@ createApp({
                 {
                     url: 'http://www.viaggiareonline.it/wp-content/uploads/2014/11/sweden_148857365.jpg',
                     title: 'Svezia',
-                    description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
+                    description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.',
                 },
             
                 {
                     url: 'https://static1.evcdn.net/images/reduction/1513757_w-1920_h-1080_q-70_m-crop.jpg',
                     title: 'Perù',
-                    description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
+                    description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.',
                 },
             
                 {
                     url: 'https://img.itinari.com/pages/images/original/0d3ed180-d22d-48e8-84df-19c4d888b41f-62-crop.jpg?ch=DPR&dpr=2.625&w=1600&s=7ebd4b5a9e045f41b4e0c7c75d298d6c',
                     title: 'Chile',
-                    description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
+                    description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.',
                 },
                 {
                     url: 'https://static1.evcdn.net/images/reduction/1583177_w-1920_h-1080_q-70_m-crop.jpg',
                     title: 'Argentina',
-                    description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
+                    description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.',
                 },
                 {
                     url: 'https://cdn.sanity.io/images/24oxpx4s/prod/ed09eff0362396772ad50ec3bfb728d332eb1c30-3200x2125.jpg?w=1600&h=1063&fit=crop',
                     title: 'Colombia',
-                    description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
+                    description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.',
                 },
             ],
             counter: 0,
             
         }
-    }
+    },
+
+    methods: {
+        // createImg(){
+        //     images.forEach((element, index) => {
+        //         const myImgTemp =  `
+        //         <div class="my-carousel-item">
+        //             <img class="img-fluid" src="${elemento.url}" alt="${elemento.title} picture">
+        //             <div class="item-description px-3">
+        //                 <h2>${elemento.title}</h2>
+        //                 <p>${elemento.description}</p>
+        //             </div>
+        //         </div>
+        //         `;
+        //     })
+            
+        // },
+
+    },
 }).mount('#app')
 
 // const myCarouselImages = document.querySelector('.my-carousel-images');
